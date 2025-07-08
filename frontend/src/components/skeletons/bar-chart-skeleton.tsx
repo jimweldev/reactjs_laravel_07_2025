b9c6961289691c4dd485954060d8b14f921098b8
Jimwel Dizon <jimweldizon.dev@gmail.com>
2025-07-08 22:22:30 +0800

    feat: simplify BarChartSkeleton component by using dynamic height mapping

--- frontend/src/components/skeletons/bar-chart-skeleton.tsx
+++ frontend/src/components/skeletons/bar-chart-skeleton.tsx
@@ -1,56 +1,31 @@
 import { Skeleton } from '../ui/skeleton';
 
+const barHeights = [
+  '10%',
+  '18%',
+  '20%',
+  '23%',
+  '24%',
+  '25%',
+  '40%',
+  '43%',
+  '63%',
+  '64%',
+  '80%',
+  '85%',
+];
+
 const BarChartSkeleton = () => {
   return (
-    <div className="grid h-[250px] grid-cols-12 gap-3 p-4">
-      <div className="col-span-1 flex flex-col justify-end gap-2">
-        <Skeleton className="h-[10%] w-full" />
-        <Skeleton className="h-4 w-full" />
-      </div>
-      <div className="col-span-1 flex flex-col justify-end gap-2">
-        <Skeleton className="h-[18%] w-full" />
-        <Skeleton className="h-4 w-full" />
-      </div>
-      <div className="col-span-1 flex flex-col justify-end gap-4">
-        <Skeleton className="h-[20%] w-full" />
-        <Skeleton className="h-4 w-full" />
-      </div>
-      <div className="col-span-1 flex flex-col justify-end gap-4">
-        <Skeleton className="h-[23%] w-full" />
-        <Skeleton className="h-4 w-full" />
-      </div>
-      <div className="col-span-1 flex flex-col justify-end gap-4">
-        <Skeleton className="h-[24%] w-full" />
-        <Skeleton className="h-4 w-full" />
-      </div>
-      <div className="col-span-1 flex flex-col justify-end gap-4">
-        <Skeleton className="h-[25%] w-full" />
-        <Skeleton className="h-4 w-full" />
-      </div>
-      <div className="col-span-1 flex flex-col justify-end gap-4">
-        <Skeleton className="h-[40%] w-full" />
-        <Skeleton className="h-4 w-full" />
-      </div>
-      <div className="col-span-1 flex flex-col justify-end gap-4">
-        <Skeleton className="h-[43%] w-full" />
-        <Skeleton className="h-4 w-full" />
-      </div>
-      <div className="col-span-1 flex flex-col justify-end gap-4">
-        <Skeleton className="h-[63%] w-full" />
-        <Skeleton className="h-4 w-full" />
-      </div>
-      <div className="col-span-1 flex flex-col justify-end gap-4">
-        <Skeleton className="h-[64%] w-full" />
-        <Skeleton className="h-4 w-full" />
-      </div>
-      <div className="col-span-1 flex flex-col justify-end gap-4">
-        <Skeleton className="h-[95%] w-full" />
-        <Skeleton className="h-4 w-full" />
-      </div>
-      <div className="col-span-1 flex flex-col justify-end gap-4">
-        <Skeleton className="h-[100%] w-full" />
-        <Skeleton className="h-4 w-full" />
-      </div>
+    <div className="grid h-[250px] grid-cols-12 gap-5 px-4 py-2">
+      {barHeights.map((height, index) => (
+        <div key={index} className="col-span-1 flex flex-col justify-end gap-3">
+          <div className="flex flex-1 items-end">
+            <Skeleton className={`h-[${height}] w-full`} />
+          </div>
+          <Skeleton className="h-4 w-full" />
+        </div>
+      ))}
     </div>
   );
 };
